e54622da384df457a80dcba640e4b0da0dc16e4d
parisfry <p.fry0220221@arts.ac.uk>
2024-12-13 09:40:03 +0000

    FINAL1.1

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
-
 let steps = 360 * 3;
-let r = 320;
+let r = 50;
 let noiseScale = 0.03;
 const noiseAmount = 500;
 
@@ -9,7 +8,7 @@ function setup() {
   noFill();
   stroke(255, 50);
   background(0);
-  r = 400 / 2.5;
+  
 }
 
 function distortedCircle() {
@@ -18,7 +17,7 @@ function distortedCircle() {
     x = width / 2 + r * cos((TWO_PI * i) / steps);
     y = height / 2 + r * sin((TWO_PI * i) / steps);
     x += map(
-      noise(noiseScale * x, noiseScale * y, frameCount / 500),0,1,-noiseAmount,noiseAmount);
+      noise(noiseScale * x, noiseScale * y, frameCount / 100),0,1,-noiseAmount,noiseAmount);
     y += map(
       noise(noiseScale * x, noiseScale * y, 1), 0,1, -noiseAmount,noiseAmount);
     vertex(x, y);
@@ -27,7 +26,10 @@ function distortedCircle() {
 }
 
 function draw() {
-  background(100,20);
-  stroke(25);
+  //background(0,1); more chill distortion and slow fading
+ //background(100,1); //slow fading not just black to white
+background (0,20);// intense moving with fading 
+
+  stroke(255);
   distortedCircle();
 }
