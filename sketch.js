97f85f056fe90a730aad1b5886afc209ad46131e
parisfry <p.fry0220221@arts.ac.uk>
2024-12-13 09:47:49 +0000

    FINAL1.3

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let steps = 360 * 3;
-let r = 50;
+let r = 320;
 let noiseScale = 0.03;
 const noiseAmount = 500;
 
@@ -8,28 +8,33 @@ function setup() {
   noFill();
   stroke(255, 50);
   background(0);
-  
+  r = 400 / 2.5;
+
 }
 
-function distortedCircle() {
+function distortedSpiral(turns = 5) {
   beginShape();
   for (let i = 0; i <= steps; i++) {
-    x = width / 2 + r * cos((TWO_PI * i) / steps);
-    y = height / 2 + r * sin((TWO_PI * i) / steps);
-    x += map(
-      noise(noiseScale * x, noiseScale * y, frameCount / 100),0,1,-noiseAmount,noiseAmount);
-    y += map(
-      noise(noiseScale * x, noiseScale * y, 1), 0,1, -noiseAmount,noiseAmount);
+  
+    let t = i / steps;
+    let spiralRadius = r * t * turns; // Expanding radius
+    let angle = TWO_PI * t * turns;
+    let x = width / 2 + spiralRadius * cos(angle);
+    let y = height / 2 + spiralRadius * sin(angle);
+    x += map(noise(noiseScale * x, noiseScale * y, frameCount / 500), 0, 1, -noiseAmount, noiseAmount);
+    y += map(noise(noiseScale * x, noiseScale * y, 1), 0, 1, -noiseAmount, noiseAmount);
+    
+    let hue = map(angle + frameCount * 0.01, 0, TWO_PI * turns, 0, 255);
+    let brightness = map(spiralRadius, 0, r * turns, 50, 255); // Brightness increases with radius
+    stroke(hue, 255, brightness, 100); // Set stroke color with transparency
+    
     vertex(x, y);
   }
   endShape();
 }
 
 function draw() {
-  //background(0,1); more chill distortion and slow fading
- //background(100,1); //slow fading not just black to white
-background (0,20);// intense moving with fading 
-
-  stroke(255);
-  distortedCircle();
+  colorMode(HSB, 255); // Use Hue, Saturation, Brightness mode for color
+  background(0,10);
+  distortedSpiral(50); // Change "3" for more or fewer spiral turns
 }
